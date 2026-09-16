7a0514467eb752a2fdaac3dc180d650fc9b9deca
Juan Puello <jpuellov@unicartagena.edu.co>
2026-09-16 10:00:50 -0500

    feat: inicializacion de proyecto Spring Boot MVC con Thymeleaf, JPA y Security

---
+++ src/main/java/com/ejercicio24/partidopoliticomvcc/PartidoPoliticoMvccApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio24.partidopoliticomvcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidoPoliticoMvccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartidoPoliticoMvccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ejercicio24/partidopoliticomvcc/PartidoPoliticoMvccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio24.partidopoliticomvcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidoPoliticoMvccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
